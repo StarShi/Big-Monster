15787d122887eef6c7298358deb4cc08fd0c2daf
StarShi <1297818940@qq.com>
2019-11-19 17:37:10 +0800

    新增测试报告

--- source/auto-test/jest.config.js
+++ source/auto-test/jest.config.js
@@ -1,10 +1,18 @@
 module.exports = {
   verbose: true,//控制台打印测试用例执行信息
-  globalSetup: './utils/setup.js',//全局启动后会执行这个文件代码，然后才运行测试用例
-  globalTeardown: './utils/teardown.js',//全部用例执行结束后,会执行这个文件代码
-  testEnvironment: './utils/puppeteerEnv.js',//测试环境
-  moduleFileExtensions:['js'],//模块文件后缀
-  roots: ['tests'],//测试根目录
-  coverageDirectory:'coverage',//覆盖率报告目录
+  globalSetup: "./utils/setup.js",//全局启动后会执行这个文件代码，然后才运行测试用例
+  globalTeardown: "./utils/teardown.js",//全部用例执行结束后,会执行这个文件代码
+  testEnvironment: "./utils/puppeteerEnv.js",//测试环境
+  moduleFileExtensions:["js"],//模块文件后缀
+  roots: ["tests"],//测试根目录
+  coverageDirectory:"coverage",//测试覆盖率or测试报告目录
   collectCoverage:true,//是否收集测试覆盖率
+  reporters: [
+    "default",
+    ["jest-html-reporters", {
+      "publicPath": "coverage/html-report",
+      "filename": "report.html",
+      "expand": true
+    }],
+  ],//测试报告
 };
