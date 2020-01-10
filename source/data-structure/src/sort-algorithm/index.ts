15884d1926fb7e2af5b99af8e1b4239c5d96b4e0
StarShi <1297818940@qq.com>
2020-01-10 18:51:03 +0800

    快速排序

--- source/data-structure/src/sort-algorithm/index.ts
+++ source/data-structure/src/sort-algorithm/index.ts
@@ -73,25 +73,31 @@ export default class Sort {
     public insert(list: any[] = []): any[] {
         let newList: any[] = deepClone(list);
         let len = newList.length;
+        let index: number;
+        let tempValue: any;
         // 默认序列第一个元素为有序序列，从第二个元素开始
         for (let i: number = 1; i < len; i++) {
-            for (let j: number = i - 1; j >= 0; j--) {
-                // 如果待插入元素逆序，则进行交换，直到不在逆序为止
-                // 与选择排序相似，选择排序选择最小（最大）的插在序列首个元素，只与序列首个元素做交换
-                // 插入排序，元素插入位置不确定，需要与有序序列中的所有元素进行比较，确定合适位置再做交换
-                // 之所以使用 j+1 而不是 j ,是因为要实时获取交换后的数据
-                if (newList[j + 1] < newList[j]) {
-                    newList[j + 1] = newList[j] + newList[j + 1];
-                    newList[j] = newList[j + 1] - newList[j];
-                    newList[j + 1] = newList[j + 1] - newList[j];
+            tempValue = newList[i];
+            index = i - 1;
+            // 与选择排序相似，选择排序选择最小（最大）的插在序列首个元素，只与序列首个元素做交换
+            // 插入排序，元素插入位置不确定，需要与有序序列中的所有元素进行比较，确定合适位置再做交换
+            // 如果待插入元素逆序，寻找待插入的位置，一次性插入
+            if (newList[index + 1] < newList[index]) {
+                while (index >= 0 && tempValue < newList[index]) {
+                    newList[index + 1] = newList[index];//进行移位
+                    index--;
                 }
+                // 因为最后一次满足条件时进行了index--，判断是否终止移位，所以在找到适合的位置时，需要对进行index + 1 
+                newList[index + 1] = tempValue;
             }
         }
         return newList
     }
 
     /**
-     * @description 希尔排序
+     * @description 希尔排序 分成 d = len / 2 组，向下取整，对每组进行直接插入排序，让其尽可能接近最终的位置；
+     *              使 d = d / 2,再次分组为上次步长的一半，重复分组与排序；
+     *              直至步长 d = 1 时，即把所有元素放在同一组中完成一次直接插入排序为止；
      * @author Star Shi
      * @date 2020-01-08
      * @param {any[]} list
@@ -103,21 +109,87 @@ export default class Sort {
         // 假设分组时，每组两个元素
         // 分成 len / 2 组，向下取整，得到步长
         // 每轮排序时，取上次步长的一半
+        // 直到步长为1时，即把所有元素放在同一组中，完成一次完整的插入排序
+        let index: number;
+        let tempValue: any;
         for (let d: number = Math.floor(len / 2); d > 0; d = Math.floor(d / 2)) {
+            // 初始化 i = d ，取间隔位置的元素向前插入
+            // 为什么取间隔位置的元素？与插入排序一样，每个分组中的第一个元素默认有序，从每个分组的第二位开始比较插入即可
             for (let i: number = d; i < len; i++) {
-                for (let j: number = i - d; j >= 0; j -= d) {
-                    // 取间隔位置的元素向前插入
-                    // 如果待插入元素逆序，则交换，直到不在逆序为止
-                    // 之所以使用 j+1 而不是 j ,是因为要实时获取交换后的数据
-                    if (newList[j + d] < newList[j]) {
-                        newList[j + d] = newList[j] + newList[j + d];
-                        newList[j] = newList[j + d] - newList[j];
-                        newList[j + d] = newList[j + d] - newList[j];
+                tempValue = newList[i];
+                index = i - d;
+                // 如果待插入元素逆序，寻找待插入的位置，一次性插入
+                if (newList[index + d] < newList[index]) {
+                    while (index >= 0 && tempValue < newList[index]) {
+                        newList[index + d] = newList[index];//移位
+                        index -= d;
                     }
+                    newList[index + d] = tempValue;
                 }
             }
-            console.log(newList)
         }
         return newList;
+    }
+
+    public quick(list: any[], start: number, end: number) {
+        if (start >= end) { return }
+        // let pivot: any = this.getPivot(list, start, end);//获取分界值
+        let pivot: any  = list[end];//末尾值为分界值
+
+        let left: number = start;
+        let right: number = end - 1;//在获取分片分界值时，数组已经将三个数中最大的一个数，放置分片的末尾，所以只需将下标减一 即可得的将right下标指向分解值
+        // 当left 与 right 不相遇时，循环进行检索
+        while (left < right) {
+            // 找到一个比分界值pivot更大的值的位置，如果没找到继续往右找
+            while (left <= right && list[left] <= pivot) {
+                left++;
+            }
+            // 找到一个比分界值pivot更小的值的位置,如果没找到继续往左找
+            while (left <= right && list[right] > pivot) {
+                right--;
+            }
+            console.log("=>", left, right, list,pivot)
+            // 找到之后，如果左指针小于右指针
+            if (left < right) {
+                // 使找到的两个值进行交换
+                this.swap(list, left, right);
+            } else {
+                break;
+            }
+        }
+        console.log(left, end)
+        // 将分界值放置在正确的位置
+        this.swap(list, left, end);
+        // 排左边
+        this.quick(list, start, left - 1);
+        // 排右边
+        this.quick(list, left + 1, end);
+    }
+
+    // 获取分界值，返回数组第一位，中间位和最后一位的中位数
+    public getPivot(list: any, start: number, end: number) {
+        let mid: number = Math.floor((start + end) / 2);
+        // 如果第一个值大于中间的值，则交换，
+        if (list[start] > list[mid]) {
+            this.swap(list, start, mid);
+        }
+        // 用前两个中最大值与最后一个值比较，如果大于则继续交换，然后得到的最后一位则是三位之中最大的
+        if (list[mid] > list[end]) {
+            this.swap(list, mid, end);
+        }
+        // 最后比较剩余两个数的大小，若是list[start] 大，则其换到 end-1 的位置
+        if (list[start] > list[mid]) {
+            this.swap(list, start, end - 1);
+        } else {// 否则将mid 换到 end-1 的位置
+            this.swap(list, mid, end - 1);
+        }
+        return list[end - 1];
+    }
+
+    // 交换
+    public swap(list: any, left: number, right: number) {
+        let temp: any = list[left];
+        list[left] = list[right];
+        list[right] = temp;
     }
 }
--- source/data-structure/tests/sort-algorithm/index.spec.ts
+++ source/data-structure/tests/sort-algorithm/index.spec.ts
@@ -1,29 +1,34 @@
 import Sort from '../../src/sort-algorithm'
 describe('test Sort', () => {
-    let sort:Sort = new Sort();
-    let arr:number[] = [4,1,3,0,5,2];
-    test('test bubble', () => { 
+    let sort: Sort = new Sort();
+    let arr: number[] = [4, 1, 3, 0, 5, 2];
+    test('test bubble', () => {
         let newArr = sort.bubble(arr);
         expect(newArr.toString()).toBe('0,1,2,3,4,5');
         expect(arr.toString()).toBe('4,1,3,0,5,2');
     });
 
-    test('test select', () => { 
+    test('test select', () => {
         let newArr = sort.select(arr);
         expect(newArr.toString()).toBe('0,1,2,3,4,5');
         expect(arr.toString()).toBe('4,1,3,0,5,2');
     });
 
-    test('test insert', () => { 
+    test('test insert', () => {
         let newArr = sort.insert(arr);
         expect(newArr.toString()).toBe('0,1,2,3,4,5');
         expect(arr.toString()).toBe('4,1,3,0,5,2');
     });
 
-    
-    test('test shell', () => { 
+    test('test shell', () => {
         let newArr = sort.shell(arr);
         expect(newArr.toString()).toBe('0,1,2,3,4,5');
         expect(arr.toString()).toBe('4,1,3,0,5,2');
+    });
+
+    test('test quick', () => {
+        let newArr = [7,3,2,8,1,9,5,4,6,10]
+        sort.quick(newArr,0,newArr.length-1);
+        // expect(newArr.toString()).toBe('0,1,2,3,4,5');
     });
 });
