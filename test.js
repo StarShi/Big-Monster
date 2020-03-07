50d60832508c939aa26c7431961c890a1ed75d3d
shixin <shixin007@foxmail.com>
2020-03-07 18:52:05 +0800

    文档更新

---
+++ test.js
@@ -0,0 +1,13 @@
+// 节流 每隔一段时间触发一次
+function throttle(wait,fn){
+    let nowTime = new Date().getTime();
+
+    if(now){
+
+    }
+    setTimeout(function(){
+        fn && fn();
+    },wait)
+}
+
+xss  
