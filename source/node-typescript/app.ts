af4c4f75acb3dfe3424fa6fc7bd69ae8466ae0c8
Star Shi <shixin007@foxmail.com>
2020-04-23 23:39:30 +0800

    目录调整

--- source/node-typescript/app.ts
+++ source/node-typescript/app.ts
@@ -1,19 +1,14 @@
 import Koa from "koa";
-import Router from "koa-router";
-import index from "./router/index";
-import api from "./router/api";
+import routers from "./routers/index";
 
 const app = new Koa();
-const router = new Router();
 // 对于任何请求，app将调用该异步函数处理请求：
 app.use(async (ctx, next) => {
     await next();
 });
 
 
-router.use("/index",index.routes());
-router.use("/api",api.routes());
-app.use(router.routes()).use(router.allowedMethods());
+app.use(routers.routes()).use(routers.allowedMethods());
 
 // 在端口3000监听:
 app.listen(3000);
---
+++ source/node-typescript/routers/index.ts
@@ -0,0 +1,11 @@
+
+/* 
+  整合所有子路由
+*/
+import Router from "koa-router";
+import Api from "./api";
+const router = new Router();
+
+
+router.use(Api.routes()).use(Api.allowedMethods());
+export default router
