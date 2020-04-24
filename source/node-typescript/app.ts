d5cff84383eb7dd99bc6809a59683b5620671282
StarShi <1297818940@qq.com>
2020-04-24 17:40:29 +0800

    更新文件目录

--- source/node-typescript/app.ts
+++ source/node-typescript/app.ts
@@ -1,5 +1,7 @@
 import Koa from "koa";
+import bodyParser from "koa-bodyparser";
 import routers from "./routers/index";
+// Sequelize orm
 
 const app = new Koa();
 // 对于任何请求，app将调用该异步函数处理请求：
@@ -7,7 +9,7 @@ app.use(async (ctx, next) => {
     await next();
 });
 
-
+app.use(bodyParser());// 处理post数据
 app.use(routers.routes()).use(routers.allowedMethods());
 
 // 在端口3000监听:
--- source/node-typescript/routers/api/news_api.ts
+++ source/node-typescript/routers/api/news_api.ts
@@ -2,9 +2,15 @@
 import Router from "koa-router";
 const router = new Router();
 
+router.prefix("/api")
 router.get("/news",(ctx)=>{
   ctx.body = "新闻"
 })
+
+router.post("/test",(ctx)=>{
+  ctx.body = ctx.request.body
+})
+
 
 
 export default router
